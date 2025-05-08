523593c8886e15c9828c9868dd38d4059fd6e02c
Zahra209925 <nabavizahra2099@gmail.com>
2025-05-08 19:06:01 +0300

    tehtävä valmis

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/Zahra209925/Fso-part4-Sovelluksen.git/",
   plugins: [react()],
 })
